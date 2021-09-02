cc1edd1cf63b1288cea9bbbec325c0dbf0d38ab6
Sipachev Igor <sipachev@sveak.com>
2021-09-02 13:07:54 +0700

    Добавил контейнер редиса

---
+++ tests/unit/Service/RedisHelperService/ComplexTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Rinsvent\DTO2Data\Tests\Service\RedisHelperService;
+
+
+use Rinsvent\RedisManagerBundle\Service\RedisHelperService;
+
+class ComplexTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \Rinsvent\RedisManagerBundle\Tests\UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSuccessFillRequestData()
+    {
+        // $this->tester->
+    }
+}
---
+++ tests/unit/Service/RedisManager/ComplexTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Rinsvent\DTO2Data\Tests\Service\RedisManager;
+
+
+class ComplexTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSuccessFillRequestData()
+    {
+
+    }
+}
